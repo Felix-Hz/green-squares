4711cda40561ecd77df2104c0902224b28cfb0c9
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2023-11-12 15:50:27 +0100

    fix: main & rdme txt

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Green squares 🟩
 
-To be honest, I just think that a lot of GitHub activity in my profile looks cool. But I'm lazy to commit stuff everyday, so this script is taking charge of that. It might even give me some funny jokes for when I go out with friends. Who knows... Well, here they go!
+To be honest, I just think that a lot of GitHub activity in my profile looks cool. But I'm lazy to commit stuff everyday, so this script is taking charge of that. It might even give me some nerdy facts to share. You will see that the API is quite crappy as well, but it suffices for this lil project. Who knows... Well, here they go!
--- src/main.ts
+++ src/main.ts
@@ -20,78 +20,10 @@
                                         /      ###/                                                        
 */
 
-import * as fs from "fs";
-import state from "./data/state.json";
-import { getRandomFact } from "./utils/api";
-
+import { updateReadme } from "./utils/updateReadme";
 // import { Octokit } from "octokit";
 // import { schedule } from "node-cron";
 
-const currentDay = state.day;
-interface RandomNumberFact {
-  number?: number;
-  date?: string;
-  text: string;
-}
-
-function textPerFactType(
-  type: string,
-  randomNumberFact: RandomNumberFact
-): string {
-  let textForReadme = "";
-
-  if (type === "math") {
-    const text = `The number ${randomNumberFact.number} is ${randomNumberFact.text}.`;
-    textForReadme = text;
-  } else if (type === "year") {
-    if (randomNumberFact.date) {
-      const text = `In ${randomNumberFact.date} of ${randomNumberFact.number}, ${randomNumberFact.text}.`;
-      textForReadme = text;
-    } else {
-      const text = `In year ${randomNumberFact.number}, ${randomNumberFact.text}.`;
-      textForReadme = text;
-    }
-  } else if (type === "trivia") {
-    const text = `The number ${randomNumberFact.number} is ${randomNumberFact.text}.`;
-    textForReadme = text;
-  }
-
-  return textForReadme;
-}
-
-async function updateReadme() {
-  let updatedContent: string;
-  const readmePath = "../README.md";
-  const randomNumberFact = await getRandomFact();
-  let type = randomNumberFact?.type;
-
-  fs.readFile(readmePath, "utf-8", async (err, data) => {
-    if (err) {
-      console.error("Error reading the file:", err);
-      return;
-    }
-
-    if (typeof type === "string" && randomNumberFact) {
-      var textForReadme = textPerFactType(type, randomNumberFact);
-      if (!currentDay) {
-        updatedContent = `${data}\n\n### Creation Day\n${textForReadme}`;
-      } else {
-        updatedContent = `${data}\n\n### Fact of Day ${currentDay}\n${textForReadme}`;
-      }
-      fs.writeFile(readmePath, updatedContent, "utf-8", (err) => {
-        if (err) {
-          console.error("Error writing to the file:", err);
-          return;
-        } else {
-          state.day += 1;
-          fs.writeFileSync("./data/state.json", JSON.stringify(state));
-        }
-      });
-    } else {
-      console.error("Nope, nopity nope.");
-    }
-  });
-}
 updateReadme();
 
 // // Schedule the job to run every 24 hours
